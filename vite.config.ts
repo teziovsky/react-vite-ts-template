7034cc234f00aee683fb581f4ba715f7ef03c015
Jakub Soboczyński <teziovsky@gmail.com>
2022-08-11 22:38:39 +0200

    feat: add eslint and prettier packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default ({ mode }) => {
+  const { APP_PORT } = loadEnv(mode, path.resolve(__dirname, ".."), "APP");
+
+  return defineConfig({
+    plugins: [react()],
+    envDir: path.resolve(__dirname, ".."),
+    server: {
+      port: parseInt(APP_PORT) || 3000,
+    },
+  });
+};
